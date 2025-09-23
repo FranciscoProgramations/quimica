645802267030aed4d01d7eb7d6e4b67d67b8c8ca
Francisco <frasa112009@gmail.com>
2025-09-23 20:33:28 -0300

    Update script.js

--- JSquimica/javascript/script.js
+++ JSquimica/javascript/script.js
@@ -1,29 +1,310 @@
-perguntas = [
-    {
-        pergunta: "Não sei o que não sei o que lá?",
-        respostas: [
-            {texto: "Opção 1", trueorfalse: false},
-            { texto: "Opção 2", trueorfalse: false},
-            {texto: "Opção 3", trueorfalse: false},
-            {texto: "Opção 4", trueorfalse: false},
-            {texto: "Opção 5", trueorfalse: true},
-        ]
-    },
-    {
-        pergunta: "Sei o que não sei o que lá?",
-        respostas: [
-            { texto: "Opção 1", trueorfalse: false},
-            {texto: "Opção 2", trueorfalse: true},
-            {texto: "Opção 3", trueorfalse: false},
-            {texto: "Opção 4", trueorfalse: false},
-            {texto: "Opção 5", trueorfalse: false},
-        ]
+
+let clicado1 = "";
+let clicado2 = "";
+let clicado3 = "";
+
+//função 1
+
+document.getElementById("button11").addEventListener("click", () => {
+    clicado1 = "button11"
+    if(clicado1 == "button11")
+    {   document.querySelector("#resposta1").style.display="block";
+        document.querySelector("#erro1").style.display="none";
+        document.querySelector("#pb2").style.display="block";
+        document.querySelector("#trueresposta1").style.display="block"
     }
-]
+    else
+    {   document.querySelector("#erro1").style.display="block";
+        document.querySelector("#resposta1").style.display="none";
+        document.querySelector("#pb2").style.display="none"
+        document.querySelector(".pabens").style.display="none"
+        document.querySelector("#pb3").style.display="none"
+        document.querySelector("#trueresposta1").style.display="none"
+        document.querySelector("#resposta3").style.display="none";
+        document.querySelector("#trueresposta2").style.display="none"
+        document.querySelector("#trueresposta3").style.display="none"
+        document.querySelector("#resposta2").style.display="none";
+        document.querySelector("#erro3").style.display="none";
+        document.querySelector("#erro2").style.display="none";
+    }});
+  
+document.getElementById("button12").addEventListener("click", () => {
+    clicado1 = "button12"
+    if(clicado1 == "button11")
+    {   document.querySelector("#resposta1").style.display="block";
+        document.querySelector("#erro1").style.display="none";
+        document.querySelector("#trueresposta1").style.display="block"
+    }
+    else
+    {   document.querySelector("#erro1").style.display="block";
+        document.querySelector("#resposta1").style.display="none";
+        document.querySelector("#pb2").style.display="none"
+        document.querySelector(".pabens").style.display="none"
+        document.querySelector("#pb3").style.display="none"
+        document.querySelector("#trueresposta1").style.display="none"
+        document.querySelector("#resposta3").style.display="none";
+        document.querySelector("#trueresposta2").style.display="none"
+        document.querySelector("#trueresposta3").style.display="none"
+        document.querySelector("#resposta2").style.display="none";
+        document.querySelector("#erro3").style.display="none";
+        document.querySelector("#erro2").style.display="none";
+    }});
 
-perguntaElemento = document.getElementById("pergunta");
-opcaoElemento = document.getElementById("button");
-proximoElemento = document.getElementById("proximo");
+document.getElementById("button13").addEventListener("click", () => {
+    clicado1 = "button13"
+    if(clicado1 == "button11")
+    {   document.querySelector("#resposta1").style.display="block";
+        document.querySelector("#erro1").style.display="none";
+        document.querySelector("#trueresposta1").style.display="block"
+    }
+    else
+    {   document.querySelector("#erro1").style.display="block";
+        document.querySelector("#resposta1").style.display="none";
+        document.querySelector("#pb2").style.display="none"
+        document.querySelector(".pabens").style.display="none"
+        document.querySelector("#pb3").style.display="none"
+        document.querySelector("#trueresposta1").style.display="none"
+        document.querySelector("#resposta3").style.display="none";
+        document.querySelector("#trueresposta2").style.display="none"
+        document.querySelector("#trueresposta3").style.display="none"
+        document.querySelector("#resposta2").style.display="none";
+        document.querySelector("#erro3").style.display="none";
+        document.querySelector("#erro2").style.display="none";
+    }});
 
-let perguntaNo = 0;
-let acerto = 0;
+document.getElementById("button14").addEventListener("click", () => {
+    clicado1 = "button14"
+    if(clicado1 == "button11")
+    {   document.querySelector("#resposta1").style.display="block";
+        document.querySelector("#erro1").style.display="none";
+        document.querySelector("#trueresposta1").style.display="block"
+    }
+    else
+    {   document.querySelector("#erro1").style.display="block";
+        document.querySelector("#resposta1").style.display="none";
+        document.querySelector("#pb2").style.display="none"
+        document.querySelector(".pabens").style.display="none"
+        document.querySelector("#pb3").style.display="none"
+        document.querySelector("#trueresposta1").style.display="none"
+        document.querySelector("#resposta3").style.display="none";
+        document.querySelector("#trueresposta2").style.display="none"
+        document.querySelector("#trueresposta3").style.display="none"
+        document.querySelector("#resposta2").style.display="none";
+        document.querySelector("#erro3").style.display="none";
+        document.querySelector("#erro2").style.display="none";
+    }});
+
+document.getElementById("button15").addEventListener("click", () => {
+    clicado1 = "button15"
+    if(clicado1 == "button11")
+    {   document.querySelector("#resposta1").style.display="block";
+        document.querySelector("#erro1").style.display="none";
+        document.querySelector("#trueresposta1").style.display="block"
+    }
+    else
+    {   document.querySelector("#erro1").style.display="block";
+        document.querySelector("#resposta1").style.display="none";
+        document.querySelector("#pb2").style.display="none"
+        document.querySelector(".pabens").style.display="none"
+        document.querySelector("#pb3").style.display="none"
+        document.querySelector("#trueresposta1").style.display="none"
+        document.querySelector("#resposta3").style.display="none";
+        document.querySelector("#trueresposta2").style.display="none"
+        document.querySelector("#trueresposta3").style.display="none"
+        document.querySelector("#resposta2").style.display="none";
+        document.querySelector("#erro3").style.display="none";
+        document.querySelector("#erro2").style.display="none";
+        
+    }});
+
+
+//função 2
+
+  document.getElementById("button21").addEventListener("click", () => {
+    if(clicado1 == "button11"){
+    clicado2 = "button21"
+      if(clicado2 == "button24")
+      {   document.querySelector("#resposta2").style.display="block";
+          document.querySelector("#erro2").style.display="none";
+          document.querySelector("#trueresposta2").style.display="block"
+      }
+      else
+      {   document.querySelector("#erro2").style.display="block";
+          document.querySelector("#resposta2").style.display="none";
+          document.querySelector("#pb3").style.display="none"
+          document.querySelector(".pabens").style.display="none"
+          document.querySelector("#trueresposta2").style.display="none"
+          document.querySelector("#trueresposta3").style.display="none"
+          document.querySelector("#resposta3").style.display="none";
+          document.querySelector("#erro3").style.display="none";
+      }  
+      }});
+
+  document.getElementById("button22").addEventListener("click", () => {
+     if(clicado1 == "button11"){
+      clicado2 = "button22"
+      if(clicado2 == "button24")
+      {   document.querySelector("#resposta2").style.display="block";
+          document.querySelector("#erro2").style.display="none";
+          document.querySelector("#trueresposta2").style.display="block"
+      }
+      else
+      {   document.querySelector("#erro2").style.display="block";
+          document.querySelector("#resposta2").style.display="none";
+          document.querySelector("#pb3").style.display="none"
+          document.querySelector(".pabens").style.display="none"
+          document.querySelector("#trueresposta2").style.display="none"
+          document.querySelector("#trueresposta3").style.display="none"
+          document.querySelector("#resposta3").style.display="none";
+          document.querySelector("#erro3").style.display="none";
+      }
+      }});
+
+  document.getElementById("button23").addEventListener("click", () => {
+     if(clicado1 == "button11"){
+      clicado2 = "button23"
+      if(clicado2 == "button24")
+      {   document.querySelector("#resposta2").style.display="block";
+          document.querySelector("#erro2").style.display="none";
+          document.querySelector("#trueresposta2").style.display="block"
+      }
+      else
+      {   document.querySelector("#erro2").style.display="block";
+          document.querySelector("#resposta2").style.display="none";
+          document.querySelector("#pb3").style.display="none"
+          document.querySelector(".pabens").style.display="none"
+          document.querySelector("#trueresposta2").style.display="none"
+          document.querySelector("#trueresposta3").style.display="none"
+          document.querySelector("#resposta3").style.display="none";
+          document.querySelector("#erro3").style.display="none";
+      }
+      }});
+
+  document.getElementById("button24").addEventListener("click", () => {
+     if(clicado1 == "button11"){
+      clicado2 = "button24"
+      if(clicado2 == "button24")
+      {   document.querySelector("#resposta2").style.display="block";
+          document.querySelector("#erro2").style.display="none";
+          document.querySelector("#pb3").style.display="block"
+          document.querySelector("#trueresposta2").style.display="block"
+      }
+      else
+      {   document.querySelector("#erro2").style.display="block";
+          document.querySelector("#resposta2").style.display="none";
+          document.querySelector("#pb3").style.display="none"
+          document.querySelector(".pabens").style.display="none"
+          document.querySelector("#trueresposta2").style.display="none"
+          document.querySelector("#trueresposta3").style.display="none"
+          document.querySelector("#resposta3").style.display="none";
+          document.querySelector("#erro3").style.display="none";
+      }
+      }});
+
+  document.getElementById("button25").addEventListener("click", () => {
+     if(clicado1 == "button11"){
+      clicado2 = "button25"
+      if(clicado2 == "button24")
+      {   document.querySelector("#resposta2").style.display="block";
+          document.querySelector("#erro2").style.display="none";
+          document.querySelector("#trueresposta2").style.display="block"
+      }
+      else
+      {   document.querySelector("#erro2").style.display="block";
+          document.querySelector("#resposta2").style.display="none";
+          document.querySelector("#pb3").style.display="none"
+          document.querySelector(".pabens").style.display="none"
+          document.querySelector("#trueresposta2").style.display="none"
+          document.querySelector("#trueresposta3").style.display="none"
+          document.querySelector("#resposta3").style.display="none";
+          document.querySelector("#erro3").style.display="none";
+      }
+      }});
+
+
+//função 3
+
+    document.getElementById("button31").addEventListener("click", () => {
+    if(clicado2 == "button24"){
+    clicado3 = "button31"
+      if(clicado3 == "button32")
+      {   document.querySelector("#resposta3").style.display="block";
+          document.querySelector("#erro3").style.display="none";
+          document.querySelector("#trueresposta3").style.display="block"
+      }
+      else
+      {   document.querySelector("#erro3").style.display="block";
+          document.querySelector("#resposta3").style.display="none";
+          document.querySelector(".pabens").style.display="none"
+          document.querySelector("#trueresposta3").style.display="none"
+      }  
+      }});
+
+      document.getElementById("button32").addEventListener("click", () => {
+    if(clicado2 == "button24"){
+    clicado3 = "button32"
+      if(clicado3 == "button32")
+      {   document.querySelector("#resposta3").style.display="block";
+          document.querySelector("#erro3").style.display="none";
+          document.querySelector(".pabens").style.display="block"
+          document.querySelector("#trueresposta3").style.display="block"
+      }
+      else
+      {   document.querySelector("#erro3").style.display="block";
+          document.querySelector("#resposta3").style.display="none";
+          document.querySelector(".pabens").style.display="none"
+          document.querySelector("#trueresposta3").style.display="none"
+      }  
+      }});
+
+      document.getElementById("button33").addEventListener("click", () => {
+    if(clicado2 == "button24"){
+    clicado3 = "button33"
+      if(clicado3 == "button32")
+      {   document.querySelector("#resposta3").style.display="block";
+          document.querySelector("#erro3").style.display="none";
+          document.querySelector("#trueresposta3").style.display="block"
+      }
+      else
+      {   document.querySelector("#erro3").style.display="block";
+          document.querySelector("#resposta3").style.display="none";
+          document.querySelector(".pabens").style.display="none"
+          document.querySelector("#trueresposta3").style.display="none"
+      }  
+      }});
+
+      document.getElementById("button34").addEventListener("click", () => {
+    if(clicado2 == "button24"){
+    clicado3 = "button34"
+      if(clicado3 == "button32")
+      {   document.querySelector("#resposta3").style.display="block";
+          document.querySelector("#erro3").style.display="none";
+          document.querySelector("#trueresposta3").style.display="block"
+      }
+      else
+      {   document.querySelector("#erro3").style.display="block";
+          document.querySelector("#resposta3").style.display="none";
+          document.querySelector(".pabens").style.display="none"
+          document.querySelector("#trueresposta3").style.display="none"
+      }  
+      }});
+
+      document.getElementById("button35").addEventListener("click", () => {
+    if(clicado2 == "button24"){
+    clicado3 = "button35"
+      if(clicado3 == "button32")
+      {   document.querySelector("#resposta3").style.display="block";
+          document.querySelector("#erro3").style.display="none";
+          document.querySelector("#trueresposta3").style.display="block"
+      }
+      else
+      {   document.querySelector("#erro3").style.display="block";
+          document.querySelector("#resposta3").style.display="none";
+          document.querySelector(".pabens").style.display="none"
+          document.querySelector("#trueresposta3").style.display="none"
+      }  
+      }});
+
+
+
+
